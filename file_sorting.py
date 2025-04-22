eced4fe30af2a0633e32210725525385d7142cbe
Gijs Entius <g.m.entius@gmail.com>
2025-04-22 22:06:07 +0200

    feat: Add adjusting modification time for files

--- file_sorting.py
+++ file_sorting.py
@@ -13,6 +13,33 @@ import shutil
 import re 
 from shutil import SameFileError
 from datetime import datetime
+from PIL import Image, ExifTags
+from pillow_heif import register_heif_opener
+import time
+
+
+register_heif_opener()
+
+SUPPORTED_EXTENSIONS = ["jpg", "jpeg", "png", "heic", "mov", "avi", "mp4"]
+
+
+class FileDate():
+    """Datastructure for handling file dates
+
+    """
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __str__(self):
+        return f"{self.year}-{self.month}-{self.day}"
+
+    def date_tuple(self):
+        return (self.year, self.month, self.day)
+
+    def date_epoch(self):
+        return time.mktime(time.strptime(str(self), "%Y-%m-%d"))
 
 
 class FileSorter():
@@ -25,36 +52,64 @@ class FileSorter():
     def __init__(self):
         self.base_dir = ""
         self.move = False
-        self.extension = "*"
 
     def _convert_timestamp(self, timestamp):
         '''Function to convert a unix timestamp to yyyy-mm-dd
         '''
         return datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d")
 
+    def _get_modified_date(self, file):
+        timestamp = self._convert_timestamp(os.stat(file).st_mtime)
+        time_array = timestamp.split("-")
+        return FileDate(time_array[0], time_array[1], time_array[2])
+
+    def _get_date_from_filename(self, file):
+        filename = file.name
+        for extension in SUPPORTED_EXTENSIONS:
+            match = re.search("^(IMG|WP|MOV|VID){0,1}[-|_]{0,1}([0-9]{8}){1}(_|-){1}.*" + extension + "$", filename)
+            if match:
+                return FileDate(match.group(2)[:4], match.group(2)[4:6], match.group(2)[6:8])
+
+    def _get_date_from_exif(self, file):
+        try:
+            img = Image.open(file.path)
+            img_exif = img.getexif()
+            for k, v in img_exif.items():
+                if k in ExifTags.TAGS and Image.ExifTags.TAGS[k] == "DateTime":
+                    img_datetime = v.split(" ")[0].split(":")
+                    return FileDate(img_datetime[0], img_datetime[1], img_datetime[2])
+        except:
+            pass
+
+    def _update_mod_date(self, file, file_date):
+        os.utime(file.path, (file_date.date_epoch(), file_date.date_epoch()))
+
+    def _adjust_file_date(self, file):
+        exif_date = self._get_date_from_exif(file)
+        name_date = self._get_date_from_filename(file)
+        mod_date = self._get_modified_date(file)
+
+        if exif_date is not None:
+            if str(exif_date) != str(mod_date):
+                self._update_mod_date(file, exif_date)
+            return exif_date.date_tuple()
+        elif name_date is not None:
+            if str(name_date) != str(mod_date):
+                self._update_mod_date(file, name_date)
+            return name_date.date_tuple()
+        else:
+            return mod_date.date_tuple()
+
     def _copy_file(self, file):
         '''Function to copy a file
         '''
-        filename = file.name
-        extension = filename.split(".")[1]
-        match = re.search("^(IMG_|WP_|MOV_|VID_){0,1}([0-9]{8}){1}_.*" + self.extension + "$", filename)
-
-        if match:
-            year = match.group(2)[:4]
-            month = match.group(2)[4:6]
-            day = match.group(2)[6:8]
-        elif not match and self.extension == "*" or extension == self.extension:
-            timestamp = self._convert_timestamp(os.stat(file).st_mtime)
-            time_array = timestamp.split("-")
-            year = time_array[0]
-            month = time_array[1]
-            day = time_array[2]
-        else:
-            print(f"No match for {file.path} is found")
+        if file.name.split(".")[1].lower() not in SUPPORTED_EXTENSIONS:
             return
 
+        year, month, day = self._adjust_file_date(file)
+
         if int(year) < 1901:
-            year = "9999"
+            year = "0001"
             month = "01"
             day = "01"
 
@@ -62,9 +117,9 @@ class FileSorter():
 
         try:
             if self.move:
-                shutil.move(file, f"{self.base_dir}{year}/{month}/{day}/{filename}")
+                shutil.move(file, f"{self.base_dir}{year}/{month}/{day}/{file.name}")
             else:
-                shutil.copy2(file, f"{self.base_dir}{year}/{month}/{day}/{filename}")
+                shutil.copy2(file, f"{self.base_dir}{year}/{month}/{day}/{file.name}")
         except SameFileError:
             pass
 
@@ -97,11 +152,7 @@ class FileSorter():
         else:
             self.base_dir = self._check_base_dir(to_dir)
         self.move = move
-        self.extension = "*" if extension == "" else extension
         self.remove_empty_dir = remove_empty_dir
 
-        if self.remove_empty_dir and self.base_dir == from_dir:
-            print("Setting from and to dir to the same directory is dangerous when cleaning up empty dirs")
-
-        print(f"Organizing files from with type {self.extension} {from_dir} to {self.base_dir} using {'move' if move else 'copy'}")
+        print(f"Organizing files from {from_dir} to {self.base_dir} using {'move' if move else 'copy'}")
         self._sort_files_recursive(from_dir)
